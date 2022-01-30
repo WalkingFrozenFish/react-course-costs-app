8aabbb4f379d904dde30df94fb28f33308616e53
WalkingFrozenFish <chachin.ol@yandex.kz>
2022-01-30 20:25:13 +0600

    Создание статичного приложения

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CostItem from './components/CostItem/CostItem';
+import Costs from './components/Costs/Costs';
 
-function App() {
+const App = () => {
+
+  const costs = [
+    {
+      date: new Date(2021, 2, 12),
+      description: "Холодильник",
+      amount: 999.99
+    },
+    {
+      date: new Date(2021, 11, 25),
+      description: "MacBook",
+      amount: 1254.72
+    },
+    {
+      date: new Date(2021, 4, 1),
+      description: "Джинсы",
+      amount: 49.99
+    },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Изучаем react</h1>
+      <Costs costs={costs} />
+      {/* <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount} />
+      <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount} />
+      <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount} /> */}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/CostItem/CostItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Card from '../Card/Card';
+import CostDate from '../CostDate/CostDate';
+import "./CostItem.css";
+
+const CostItem = (props) => {
+    // console.log(props)
+    return (
+        <Card className='cost-item'>
+            <CostDate date={props.date} />
+            <div className='cost-item__description'>
+                <h2>{props.description}</h2>
+                <div className='cost-item__price'>${props.amount}</div>
+            </div>
+        </Card>
+    );
+};
+
+export default CostItem;
